7616ee9e5c6a0dffebfea96db9d5093307c4501a
Maksim Murzin <maks_murzin@yahoo.com>
2021-02-13 20:02:54 +0000

    initial commit, all seems to be working

---
+++ app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+
+let date = new Date();
+let today = date.toLocaleDateString("en-US", options);
+
+let items = ["eat", "sleep", "repeat"];
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("list", { listOfItems: items, dateToday: today });
+});
+
+app.post("/", (req, res) => {
+  let newToDO = req.body.todoTask;
+  items.push(newToDO);
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
